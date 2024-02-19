486ebfc80e086975413f9e69ba314b9ff6657bc6
naija-geek <onlineweb.spport@gmail.com>
2024-02-19 01:24:02 +0100

    feat: added new c files

---
+++ scan.c
@@ -0,0 +1,16 @@
+#include <stdio.h>;
+
+int main(){
+    char fname[25];
+    // char sname[25];
+
+    printf("What is your first name?");
+    scanf("%s", &fname);
+    // printf("What is your second name?");
+    // scanf("%s", &sname);
+    // fgets(fname, 25, stdin);
+    //fgets(name, 10, stdin);
+
+    printf("Your name is %s", fname);
+    return 0;
+}
